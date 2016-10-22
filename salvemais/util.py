2da5d1a1596c6992283cdd42938c67f1a0450dc2
Michael Rabbit <michael.coelho@iclinic.com.br>
2016-10-22 13:33:35 -0200

    utils dictionary identation

--- salvemais/util.py
+++ salvemais/util.py
@@ -1,14 +1,40 @@
 
 # Receiver to Donor
 cell_chart = {
-    'A':  {'A': True,  'B': False, 'AB': False, 'O': True},
-    'B':  {'A': False, 'B': True,  'AB': False, 'O': True},
-    'AB': {'A': True,  'B': True,  'AB': True,  'O': True},
-    'O':  {'A': False, 'B': False, 'AB': False, 'O': True},
-              }
+    'A': {
+        'A': True,
+        'B': False,
+        'AB': False,
+        'O': True,
+    },
+    'B': {
+        'A': False,
+        'B': True,
+        'AB': False,
+        'O': True,
+    },
+    'AB': {
+        'A': True,
+        'B': True,
+        'AB': True,
+        'O': True,
+    },
+    'O': {
+        'A': False,
+        'B': False,
+        'AB': False,
+        'O': True,
+    },
+}
 
 # Receiver to Donor
 protein_chart = {
-    '+': {'+': True, '-': True},
-    '-': {'+': False, '-': True},
-                 }
+    '+': {
+        '+': True,
+        '-': True
+    },
+    '-': {
+        '+': False,
+        '-': True
+    },
+}
