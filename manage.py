6075d74ace5d39371cb8425d064659ae89cde156
Joao Daher Neto <joao.daher.neto@gmail.com>
2016-10-23 14:10:18 -0200

    Fixed manage arguments

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from salvemais import create_app
 
 if __name__ == '__main__':
     try:
-        env_name = sys.argv[2]
+        env_name = sys.argv.pop(2)
     except:
         env_name = None
     app = create_app(env=env_name)
