679c74db2d93df49dd62b235c5b0a07a78145d5e
Michael Rabbit <michael.coelho@iclinic.com.br>
2016-10-23 17:12:03 -0200

    Fix identation

--- salvemais/util/maps.py
+++ salvemais/util/maps.py
@@ -3,20 +3,19 @@ from flask import current_app
 
 
 class GoogleMaps:
+
     @classmethod
     def get_latlong(cls, address):
-        params = {'address': address,
-                  'key': current_app.config['GOOGLE']['maps_api_key']}
+        params = {
+            'address': address,
+            'key': current_app.config['GOOGLE']['maps_api_key'],
+        }
         url = "https://maps.googleapis.com/maps/api/geocode/json"
-
         info = requests.get(url, params=params).json()
 
         if info['results']:
             location = info['results'][0]['geometry']['location']
-            info = {
-                'lat': location['lat'],
-                'lng': location['lng']
-                    }
+            info = {'lat': location['lat'], 'lng': location['lng']}
         else:
             info = {}
         return info
